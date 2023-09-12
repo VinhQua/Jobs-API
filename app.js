22abcda7083df9880b005b30dc9c25cfef78d4e9
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-09-13 00:13:25 +0700

    security packages

--- app.js
+++ app.js
@@ -8,9 +8,27 @@ const authMiddleware = require("./middlewares/auth");
 const auth = require("./routers/auth");
 const job = require("./routers/job");
 const app = express();
+//extra security packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
 const port = process.env.PORT || 3000;
+app.set("trust proxy", true);
 //middlewares
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+    standardHeaders: "draft-7", // Set `RateLimit` and `RateLimit-Policy` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    // store: ... , // Use an external store for more precise rate limiting
+  })
+);
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 //routes
 
 //auth route
