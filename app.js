30c6a1a2b154eca38c3a29456fb6b301b3cf7727
VinhQua <dragonlinuxnoinochi@gmai.com>
2023-09-20 10:01:40 +0700

    API docs

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const authMiddleware = require("./middlewares/auth");
 const auth = require("./routers/auth");
 const job = require("./routers/job");
 const app = express();
+//Swagger UI
+
+const swaggerUI = require("swagger-ui-express");
+const yaml = require("yamljs");
+const SwaggerDocs = yaml.load("./swagger.yaml");
 //extra security packages
 const helmet = require("helmet");
 const cors = require("cors");
@@ -29,9 +34,12 @@ app.use(express.json());
 app.use(helmet());
 app.use(cors());
 app.use(xss());
-app.use(express.static("./public"));
+//API DOCS
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(SwaggerDocs));
 //routes
-
+app.get("/", (req, res) =>
+  res.send(`  <h1>Jobs API <a href="api-docs"> Documentation </a> </h1>`)
+);
 //auth route
 app.use("/api/v1/auth", auth);
 //job route
